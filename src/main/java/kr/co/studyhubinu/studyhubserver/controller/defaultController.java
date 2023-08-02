74bece878967accd6c9dd789307a6f83310e457d
unknown <dudqk9696@naver.com>
2023-08-02 16:09:19 +0900

    [Test] : default controller 만들어서 cd 테스트

---
+++ src/main/java/kr/co/studyhubinu/studyhubserver/controller/defaultController.java
@@ -0,0 +1,13 @@
+package kr.co.studyhubinu.studyhubserver.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class defaultController {
+
+    @GetMapping("/")
+    public String home() {
+        return "<h1>aws<h1>";
+    }
+}
