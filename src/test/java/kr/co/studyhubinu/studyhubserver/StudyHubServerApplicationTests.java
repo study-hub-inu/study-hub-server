c1ef372534ebf75aa437885ee55788fe1843a08a
wellbeing-dough <kdw990202@naver.com>
2023-08-09 01:14:06 +0900

    [Fix] : 테스트코드 주석처리
    추후 테스트 코드 작성 후 추가 예정

--- src/test/java/kr/co/studyhubinu/studyhubserver/StudyHubServerApplicationTests.java
+++ src/test/java/kr/co/studyhubinu/studyhubserver/StudyHubServerApplicationTests.java
@@ -3,7 +3,7 @@ package kr.co.studyhubinu.studyhubserver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class StudyHubServerApplicationTests {
 
     @Test
