a612e0e5f2e96689ad34710dc33291519e8b0635
unknown <dudqk9696@naver.com>
2023-08-09 22:00:17 +0900

    [Refactor] : defaultController 스웨거 적용 해제

--- src/main/java/kr/co/studyhubinu/studyhubserver/controller/defaultController.java
+++ src/main/java/kr/co/studyhubinu/studyhubserver/controller/defaultController.java
@@ -2,11 +2,13 @@ package kr.co.studyhubinu.studyhubserver.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 public class defaultController {
 
     @GetMapping("/")
+    @ApiIgnore
     public String home() {
         return "<h1>studyHub<h1>";
     }
