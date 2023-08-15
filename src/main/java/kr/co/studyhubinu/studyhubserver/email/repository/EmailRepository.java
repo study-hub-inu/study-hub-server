2237df9bd298c90b897517cc847e205f748d1a62
wellbeing-dough <kdw990202@naver.com>
2023-08-16 01:49:15 +0900

    [Fix] : 이메일 엔티티 삭제
    캐시를 사용하기 때문에 삭제

--- src/main/java/kr/co/studyhubinu/studyhubserver/email/repository/EmailRepository.java
+++
@@ -1,7 +0,0 @@
-package kr.co.studyhubinu.studyhubserver.email.repository;
-
-import kr.co.studyhubinu.studyhubserver.email.domain.Email;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmailRepository extends JpaRepository<Email, Long> {
-}
