fb38be82e57a24d7915606276a496a77ab06fd6e
wellbeing-dough <kdw990202@naver.com>
2023-08-16 15:43:59 +0900

    [Refact] : 메일 주소 환경변수

--- src/main/java/kr/co/studyhubinu/studyhubserver/email/service/EmailService.java
+++ src/main/java/kr/co/studyhubinu/studyhubserver/email/service/EmailService.java
@@ -4,6 +4,7 @@ import kr.co.studyhubinu.studyhubserver.email.dto.data.MailInfo;
 import kr.co.studyhubinu.studyhubserver.email.dto.data.ValidMailInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -22,15 +23,17 @@ public class EmailService {
     private final JavaMailSender emailSender;
     private final SpringTemplateEngine templateEngine;
     private final EmailCacheService emailCacheService;
+    @Value("${mail.address}")
+    private String emailAddress;
 
     //이메일 보낼 양식
     public MimeMessage createEmailForm(String email) throws MessagingException {
 
         String authCode = emailCacheService.getAndCacheAuthCode(email); // 캐시된 인증 코드 가져오기
-        String setFrom = "inustudyhub@gmail.com"; //email-config 에 설정한 자신의 이메일 주소(보내는 사람)
+        String setFrom = emailAddress; //email-config 에 설정한 자신의 이메일 주소(보내는 사람)
         String toEmail = email; //받는 사람
         String title = "스터디허브 이메일 인증 번호"; //제목
-        log.info("**************************발급된 인증 코드다" + authCode);
+        log.info("**************************발급된 인증 코드" + authCode);
 
 
         MimeMessage message = emailSender.createMimeMessage();
@@ -63,7 +66,7 @@ public class EmailService {
         String cachedAuthCode = emailCacheService.getAndCacheAuthCode(info.getEmail()); // 캐시된 인증 코드 가져오기
 
         log.info("**************************캐시된 인증 코드다" + cachedAuthCode);
-        log.info("**************************니가준 인증 코드다" + info.getAuthCode());
+        log.info("**************************입력된 인증 코드다" + info.getAuthCode());
 
 
         if (cachedAuthCode != null && cachedAuthCode.equals(info.getAuthCode())) {
