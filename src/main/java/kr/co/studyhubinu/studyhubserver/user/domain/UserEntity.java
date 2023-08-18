dcfb9ad2e8d1fb9410fe3c302cec4340e293c150
wellbeing-dough <kdw990202@naver.com>
2023-08-19 00:12:54 +0900

    [Refact] : 메시지 수정

--- src/main/java/kr/co/studyhubinu/studyhubserver/user/domain/UserEntity.java
+++ src/main/java/kr/co/studyhubinu/studyhubserver/user/domain/UserEntity.java
@@ -15,7 +15,7 @@ import javax.persistence.Id;
 
 @Entity
 @Getter
-@NoArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserEntity extends BaseTimeEntity {
 
     @Id
