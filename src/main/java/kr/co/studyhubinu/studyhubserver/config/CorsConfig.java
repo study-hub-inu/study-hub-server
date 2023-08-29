1332cc42f0c46d7a7c4201ce562c1f4325e96cf8
unknown <dudqk9696@naver.com>
2023-08-29 20:25:29 +0900

    [Refactor] : CORS 정책 변경

--- src/main/java/kr/co/studyhubinu/studyhubserver/config/CorsConfig.java
+++ src/main/java/kr/co/studyhubinu/studyhubserver/config/CorsConfig.java
@@ -3,23 +3,24 @@ package kr.co.studyhubinu.studyhubserver.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class CorsConfig {
 
     @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
 
-        source.registerCorsConfiguration("/api/**", config);
-        return new CorsFilter(source);
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
