feb92424692f9121b225ea5535ff770e5d634ea4
unknown <dudqk9696@naver.com>
2023-09-06 15:33:25 +0900

    [Refactor] : JWT 관련 코드 리팩토링

--- src/main/java/kr/co/studyhubinu/studyhubserver/config/jwt/JwtController.java
+++ src/main/java/kr/co/studyhubinu/studyhubserver/config/jwt/JwtController.java
@@ -1,7 +1,6 @@
 package kr.co.studyhubinu.studyhubserver.config.jwt;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
