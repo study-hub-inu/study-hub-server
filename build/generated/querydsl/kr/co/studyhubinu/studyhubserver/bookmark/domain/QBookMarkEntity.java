1ae488839e7f381ffa3de75b8dd18513cb353bcc
unknown <dudqk9696@naver.com>
2023-09-25 18:44:28 +0900

    [Refactor] : 롬복 어노테이션 추가

---
+++ build/generated/querydsl/kr/co/studyhubinu/studyhubserver/bookmark/domain/QBookMarkEntity.java
@@ -0,0 +1,49 @@
+package kr.co.studyhubinu.studyhubserver.bookmark.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QBookMarkEntity is a Querydsl query type for BookMarkEntity
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QBookMarkEntity extends EntityPathBase<BookMarkEntity> {
+
+    private static final long serialVersionUID = 1763927877L;
+
+    public static final QBookMarkEntity bookMarkEntity = new QBookMarkEntity("bookMarkEntity");
+
+    public final kr.co.studyhubinu.studyhubserver.common.domain.QBaseTimeEntity _super = new kr.co.studyhubinu.studyhubserver.common.domain.QBaseTimeEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdDate = _super.createdDate;
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedDate = _super.modifiedDate;
+
+    public final NumberPath<Long> postId = createNumber("postId", Long.class);
+
+    public final NumberPath<Long> userId = createNumber("userId", Long.class);
+
+    public QBookMarkEntity(String variable) {
+        super(BookMarkEntity.class, forVariable(variable));
+    }
+
+    public QBookMarkEntity(Path<? extends BookMarkEntity> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QBookMarkEntity(PathMetadata metadata) {
+        super(BookMarkEntity.class, metadata);
+    }
+
+}
+
