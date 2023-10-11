54b4ac1cc76e12226f2fff5673ced11dc96fb917
elyudwo <dudqk9696@naver.com>
2023-10-11 17:46:23 +0900

    [Fix] : 삭제된 QueryDsl 참조 안하도록 수정

--- src/main/java/kr/co/studyhubinu/studyhubserver/config/CorsConfig.java
+++ src/main/java/kr/co/studyhubinu/studyhubserver/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package kr.co.studyhubinu.studyhubserver.config;
 
-import kr.co.studyhubinu.studyhubserver.user.domain.QUserEntity;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
