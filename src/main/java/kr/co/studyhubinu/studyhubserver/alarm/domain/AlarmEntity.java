2baedf1fcd7dd6af34a3277a971eb82b3fe06c03
wellbeing-dough <kdw990202@naver.com>
2023-10-16 14:14:15 +0900

    [Feat] : 알람 관련된 엔티티, 쿼리문

---
+++ src/main/java/kr/co/studyhubinu/studyhubserver/alarm/domain/AlarmEntity.java
@@ -0,0 +1,35 @@
+package kr.co.studyhubinu.studyhubserver.alarm.domain;
+
+import kr.co.studyhubinu.studyhubserver.alarm.enums.AlarmType;
+import kr.co.studyhubinu.studyhubserver.common.domain.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "alarm")
+public class AlarmEntity extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "alarm_id")
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "post_id")
+    private Long postId;
+
+    @Column(name = "alarm_category")
+    private AlarmType alarmCategory;
+
+    private boolean checked;
+
+}
