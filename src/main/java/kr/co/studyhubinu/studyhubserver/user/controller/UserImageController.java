841bf9f1e0ac9164d12b267dc2b3d4518deaadc1
elyudwo <dudqk9696@naver.com>
2023-10-19 11:55:59 +0900

    [Feat] : 1MB 이상 이미지 에러 처리 기능 구현 (Controller, Service)

--- src/main/java/kr/co/studyhubinu/studyhubserver/user/controller/UserImageController.java
+++ src/main/java/kr/co/studyhubinu/studyhubserver/user/controller/UserImageController.java
@@ -1,15 +1,14 @@
 package kr.co.studyhubinu.studyhubserver.user.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import kr.co.studyhubinu.studyhubserver.user.dto.data.UserId;
 import kr.co.studyhubinu.studyhubserver.user.service.UserImageService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.catalina.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -17,13 +16,17 @@ import java.io.IOException;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users/image")
+@Slf4j
 public class UserImageController {
 
     private final UserImageService userImageService;
 
+    @Operation(summary = "회원 사진 저장", description = "jwt 토큰 bearer 헤더에" +
+            "보내주시면 됩니다")
     @PostMapping("")
-    public ResponseEntity<HttpStatus> uploadImage(UserId userId, MultipartFile image) throws IOException {
+    public ResponseEntity<HttpStatus> uploadImage(UserId userId, @RequestPart(name = "image", required = false) MultipartFile image) throws IOException {
         userImageService.uploadUserImage(userId.getId() ,image);
+
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
--- src/main/java/kr/co/studyhubinu/studyhubserver/user/service/UserImageService.java
+++ src/main/java/kr/co/studyhubinu/studyhubserver/user/service/UserImageService.java
@@ -2,18 +2,23 @@ package kr.co.studyhubinu.studyhubserver.user.service;
 
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import kr.co.studyhubinu.studyhubserver.exception.image.ImageSizeException;
 import kr.co.studyhubinu.studyhubserver.exception.user.UserNotFoundException;
 import kr.co.studyhubinu.studyhubserver.user.domain.UserEntity;
 import kr.co.studyhubinu.studyhubserver.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
+@Transactional
 public class UserImageService {
     private final UserRepository userRepository;
     private final AmazonS3 amazonS3;
@@ -24,6 +29,7 @@ public class UserImageService {
 
     public void uploadUserImage(Long userId, MultipartFile multipartFile) throws IOException {
         UserEntity user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+        if(multipartFile.getSize() >= 1024 * 1024) { throw new ImageSizeException(); }
         user.updateImage(saveImage(multipartFile));
     }
 
