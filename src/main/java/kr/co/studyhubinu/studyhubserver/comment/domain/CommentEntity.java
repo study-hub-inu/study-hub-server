80e1a8db18183d8a90967e4d1e2eb1db9b8f6455
wellbeing-dough <kdw990202@naver.com>
2023-11-15 01:25:03 +0900

    [Feat] : 댓글 엔티티

---
+++ src/main/java/kr/co/studyhubinu/studyhubserver/comment/domain/CommentEntity.java
@@ -0,0 +1,38 @@
+package kr.co.studyhubinu.studyhubserver.comment.domain;
+
+import kr.co.studyhubinu.studyhubserver.common.domain.BaseTimeEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "comment")
+public class CommentEntity extends BaseTimeEntity {
+
+    @Id
+    @Column(name = "comment_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "post_id")
+    private Long postId;
+
+    private String content;
+
+
+    @Builder
+    public CommentEntity(Long id, Long userId, Long postId, String content) {
+        this.id = id;
+        this.userId = userId;
+        this.postId = postId;
+        this.content = content;
+    }
+}
