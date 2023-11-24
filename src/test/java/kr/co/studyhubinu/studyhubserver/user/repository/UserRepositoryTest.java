784b2528ef9a9f753a006db900ea7545b94cf1aa
wellbeing-dough <kdw990202@naver.com>
2023-11-24 20:40:34 +0900

    [Feat] : 로컬에서 테스트 시 dev

--- src/test/java/kr/co/studyhubinu/studyhubserver/user/repository/UserRepositoryTest.java
+++ src/test/java/kr/co/studyhubinu/studyhubserver/user/repository/UserRepositoryTest.java
@@ -7,13 +7,13 @@ import kr.co.studyhubinu.studyhubserver.user.domain.UserEntity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @RepositoryTest
-@TestPropertySource(locations = "classpath:application-ci.yml")
+@ActiveProfiles("dev")
 class UserRepositoryTest {
 
     @Autowired
