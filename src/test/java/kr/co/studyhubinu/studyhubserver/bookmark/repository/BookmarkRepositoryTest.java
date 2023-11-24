b5a9be04df0f3f9c383502ad6efc05af3bcff088
wellbeing-dough <kdw990202@naver.com>
2023-11-25 03:13:31 +0900

    [Test] : 유저의_식별자로_북마크_개수를_조회한다

--- src/test/java/kr/co/studyhubinu/studyhubserver/bookmark/repository/BookmarkRepositoryTest.java
+++ src/test/java/kr/co/studyhubinu/studyhubserver/bookmark/repository/BookmarkRepositoryTest.java
@@ -7,11 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RepositoryTest
 @ActiveProfiles("dev")
@@ -21,7 +21,7 @@ public class BookmarkRepositoryTest {
     private BookmarkRepository bookMarkRepository;
 
     @Test
-    void 유저의_식별자로_북마크_리스트를_조회한다 () {
+    void 유저의_식별자로_북마크_리스트를_조회한다() {
         // given
         BookmarkEntity bookmark1 = BookmarkEntityFixture.BOOKMARK_POST1_USER1.bookMarkEntity_생성(1L);
         BookmarkEntity bookmark2 = BookmarkEntityFixture.BOOKMARK_POST2_USER1.bookMarkEntity_생성(2L);
@@ -37,4 +37,17 @@ public class BookmarkRepositoryTest {
         );
     }
 
+    @Test
+    void 유저의_식별자로_북마크_개수를_조회한다() {
+        // given
+        BookmarkEntity bookmark1 = BookmarkEntityFixture.BOOKMARK_POST1_USER1.bookMarkEntity_생성(1L);
+        BookmarkEntity bookmark2 = BookmarkEntityFixture.BOOKMARK_POST2_USER1.bookMarkEntity_생성(2L);
+        bookMarkRepository.save(bookmark1);
+        bookMarkRepository.save(bookmark2);
+        // when
+        Long actualCount = bookMarkRepository.countByUserId(1L);
+        Long expectedCount = 2L;
+        // then
+        assertEquals(expectedCount, actualCount);
+    }
 }
