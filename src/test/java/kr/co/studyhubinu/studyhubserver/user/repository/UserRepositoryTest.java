97f190221bb14997bf6f590b8f8e68a90d2f1ec2
wellbeing-dough <kdw990202@naver.com>
2023-11-25 00:04:19 +0900

    [Test] : @ActiveProfiles("dev")삭제

--- src/test/java/kr/co/studyhubinu/studyhubserver/user/repository/UserRepositoryTest.java
+++ src/test/java/kr/co/studyhubinu/studyhubserver/user/repository/UserRepositoryTest.java
@@ -13,7 +13,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @RepositoryTest
-@ActiveProfiles("dev")
 class UserRepositoryTest {
 
     @Autowired
