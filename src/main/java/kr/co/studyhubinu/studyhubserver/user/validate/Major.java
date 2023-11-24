fe0cf993911f3b6e8a828be46d84bdd5f25cb858
elyudwo <dudqk9696@naver.com>
2023-11-24 21:02:42 +0900

    [Feat] : 전공 유효성 검사 기능 구현

---
+++ src/main/java/kr/co/studyhubinu/studyhubserver/user/validate/Major.java
@@ -0,0 +1,16 @@
+package kr.co.studyhubinu.studyhubserver.user.validate;
+
+import kr.co.studyhubinu.studyhubserver.email.validate.InuEmailValidator;
+
+import javax.validation.Constraint;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = MajorValidator.class)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Major {
+
+    String message() default "존재하지 않는 전공입니다.";
+
+}
