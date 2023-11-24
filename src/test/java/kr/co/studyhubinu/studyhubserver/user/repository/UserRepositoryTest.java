2837c0a5707f65aff02931ef8eea47580492aa35
wellbeing-dough <kdw990202@naver.com>
2023-11-25 01:48:12 +0900

    [Test] : ci test

--- src/test/java/kr/co/studyhubinu/studyhubserver/user/repository/UserRepositoryTest.java
+++ src/test/java/kr/co/studyhubinu/studyhubserver/user/repository/UserRepositoryTest.java
@@ -13,7 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @RepositoryTest
-@ActiveProfiles("ci")
+@ActiveProfiles("dev")
 class UserRepositoryTest {
 
 
