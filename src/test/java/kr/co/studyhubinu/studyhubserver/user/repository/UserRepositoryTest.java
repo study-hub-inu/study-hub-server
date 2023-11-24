2530632087263553950f24fb9f0b3dd0b36e4c59
wellbeing-dough <kdw990202@naver.com>
2023-11-25 00:30:36 +0900

    [Test] : ActiceProfile 삭제

--- src/test/java/kr/co/studyhubinu/studyhubserver/user/repository/UserRepositoryTest.java
+++ src/test/java/kr/co/studyhubinu/studyhubserver/user/repository/UserRepositoryTest.java
@@ -7,13 +7,11 @@ import kr.co.studyhubinu.studyhubserver.user.domain.UserEntity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @RepositoryTest
-@ActiveProfiles("dev")
 class UserRepositoryTest {
 
     @Autowired
