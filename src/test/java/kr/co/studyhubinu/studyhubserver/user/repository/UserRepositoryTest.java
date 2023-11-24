b6c857dd1791b47512d116b2202da2e0d0240537
wellbeing-dough <kdw990202@naver.com>
2023-11-24 20:36:38 +0900

    [Fix] : ci 서버 배포 스크립트 작성

--- src/test/java/kr/co/studyhubinu/studyhubserver/user/repository/UserRepositoryTest.java
+++ src/test/java/kr/co/studyhubinu/studyhubserver/user/repository/UserRepositoryTest.java
@@ -7,11 +7,13 @@ import kr.co.studyhubinu.studyhubserver.user.domain.UserEntity;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @RepositoryTest
+@TestPropertySource(locations = "classpath:application-ci.yml")
 class UserRepositoryTest {
 
     @Autowired
