9c06ecfe910737b9040ad5558566b7b670b5810d
wellbeing-dough <kdw990202@naver.com>
2023-11-25 00:54:46 +0900

    [Test] : activeprofile 설정 변경

--- src/test/java/kr/co/studyhubinu/studyhubserver/user/repository/UserRepositoryTest.java
+++ src/test/java/kr/co/studyhubinu/studyhubserver/user/repository/UserRepositoryTest.java
@@ -13,7 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @RepositoryTest
-@ActiveProfiles("dev")
+@ActiveProfiles("prod")
 class UserRepositoryTest {
 
     @Autowired
