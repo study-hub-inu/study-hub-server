2ff6d1723dc76491b4da4b65f502f94ef7d67159
wellbeing-dough <kdw990202@naver.com>
2023-11-29 01:10:46 +0900

    [Fix] : 스터디 단건조회에 chatUrl반환

--- src/main/java/kr/co/studyhubinu/studyhubserver/studypost/dto/data/PostData.java
+++ src/main/java/kr/co/studyhubinu/studyhubserver/studypost/dto/data/PostData.java
@@ -23,12 +23,13 @@ public class PostData {
     private final String penaltyWay;
     private final LocalDate studyStartDate;
     private final LocalDate studyEndDate;
+    private final String charUrl;
     private final int remainingSeat;
     private final boolean isUsersPost;
     private final boolean isBookmarked;
     private final UserData postedUser;
 
-    public PostData(Long postId, String title, LocalDateTime createdDate, String content, MajorType major, int studyPerson, GenderType filteredGender, StudyWayType studyWay, int penalty, String penaltyWay, LocalDate studyStartDate, LocalDate studyEndDate, int remainingSeat, boolean isUsersPost, boolean isBookmarked, UserData postedUser) {
+    public PostData(Long postId, String title, LocalDateTime createdDate, String content, MajorType major, int studyPerson, GenderType filteredGender, StudyWayType studyWay, int penalty, String penaltyWay, LocalDate studyStartDate, LocalDate studyEndDate, String charUrl, int remainingSeat, boolean isUsersPost, boolean isBookmarked, UserData postedUser) {
         this.postId = postId;
         this.title = title;
         this.createdDate = createdDate;
@@ -41,12 +42,10 @@ public class PostData {
         this.penaltyWay = penaltyWay;
         this.studyStartDate = studyStartDate;
         this.studyEndDate = studyEndDate;
+        this.charUrl = charUrl;
         this.remainingSeat = remainingSeat;
         this.isUsersPost = isUsersPost;
         this.isBookmarked = isBookmarked;
         this.postedUser = postedUser;
     }
-
-//    아이디ㅇ, 생성일ㅇ, 관련학과ㅇ, 제목ㅇ, 팀원수ㅇ, 벌금 종류ㅇ, 성별ㅇ, 내용ㅇ, 시작날짜ㅇ, 종료날짜ㅇ, 벌금ㅇ,
-//    대면 여부ㅇ, 관련학과ㅇ, 작성자 아이디ㅇ, 작성자 학과d, 작성자 이름d, 작성자 프사d, 북마크 여부d, 이 글과 비슷한 스터디, 잔여석ㅇ, 작성자 여부ㅇ
 }
