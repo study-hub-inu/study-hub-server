9b1fe6ec9b66a3a5d29cc422114c4b3a5ca5f918
wellbeing-dough <kdw990202@naver.com>
2023-12-03 01:41:57 +0900

    [Refact] : 불필요한 import제거

--- src/main/java/kr/co/studyhubinu/studyhubserver/studypost/service/StudyPostService.java
+++ src/main/java/kr/co/studyhubinu/studyhubserver/studypost/service/StudyPostService.java
@@ -1,19 +1,16 @@
 package kr.co.studyhubinu.studyhubserver.studypost.service;
 
-import kr.co.studyhubinu.studyhubserver.bookmark.repository.BookmarkRepository;
 import kr.co.studyhubinu.studyhubserver.exception.study.PostNotFoundException;
 import kr.co.studyhubinu.studyhubserver.exception.user.UserNotFoundException;
 import kr.co.studyhubinu.studyhubserver.studypost.domain.StudyPostEntity;
 import kr.co.studyhubinu.studyhubserver.studypost.domain.StudyPostValidator;
-import kr.co.studyhubinu.studyhubserver.studypost.dto.data.*;
-import kr.co.studyhubinu.studyhubserver.studypost.dto.response.*;
+import kr.co.studyhubinu.studyhubserver.studypost.dto.data.StudyPostInfo;
+import kr.co.studyhubinu.studyhubserver.studypost.dto.data.UpdateStudyPostInfo;
 import kr.co.studyhubinu.studyhubserver.studypost.repository.StudyPostRepository;
 import kr.co.studyhubinu.studyhubserver.user.domain.UserEntity;
 import kr.co.studyhubinu.studyhubserver.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,7 +23,6 @@ public class StudyPostService {
     private final StudyPostRepository studyPostRepository;
     private final UserRepository userRepository;
     private final StudyPostValidator studyPostValidator;
-    private final BookmarkRepository bookMarkRepository;
 
 
     public Long createPost(StudyPostInfo info) {
