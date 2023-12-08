0f44576bf070cb196ed1e443e93a1469243c7c98
wellbeing-dough <kdw990202@naver.com>
2023-12-09 03:03:42 +0900

    [Feat] : 스캐쥴, 배치 config클래스 분리

--- src/main/java/kr/co/studyhubinu/studyhubserver/StudyHubServerApplication.java
+++ src/main/java/kr/co/studyhubinu/studyhubserver/StudyHubServerApplication.java
@@ -6,8 +6,6 @@ import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-//@EnableBatchProcessing
-//@EnableScheduling
 public class StudyHubServerApplication {
 
     public static void main(String[] args) {
--- src/main/java/kr/co/studyhubinu/studyhubserver/config/StudyPostJobConfig.java
+++ src/main/java/kr/co/studyhubinu/studyhubserver/config/StudyPostJobConfig.java
@@ -14,7 +14,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Slf4j
 @Configuration
-@EnableBatchProcessing
 @RequiredArgsConstructor
 public class StudyPostJobConfig {
 
