21745b4bb39aaa50cb993feb0571523f1a94ffd2
wellbeing-dough <kdw990202@naver.com>
2023-12-09 01:45:10 +0900

    [Feat] : batch 설정 yml에 추가

--- src/main/java/kr/co/studyhubinu/studyhubserver/StudyHubServerApplication.java
+++ src/main/java/kr/co/studyhubinu/studyhubserver/StudyHubServerApplication.java
@@ -1,15 +1,13 @@
 package kr.co.studyhubinu.studyhubserver;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableCaching
-@EnableBatchProcessing
-@EnableScheduling
+//@EnableBatchProcessing
+//@EnableScheduling
 public class StudyHubServerApplication {
 
     public static void main(String[] args) {
