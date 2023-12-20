5d2f73822fb6f24bafdeed2357b95b70d284d6f6
wellbeing-dough <kdw990202@naver.com>
2023-12-20 20:49:16 +0900

    [Test] : 북마크_아이디로_북마크_조회되면_북마크를_삭제한다

--- src/test/java/kr/co/studyhubinu/studyhubserver/bookmark/service/BookmarkServiceTest.java
+++ src/test/java/kr/co/studyhubinu/studyhubserver/bookmark/service/BookmarkServiceTest.java
@@ -4,6 +4,7 @@ import kr.co.studyhubinu.studyhubserver.bookmark.domain.BookmarkEntity;
 import kr.co.studyhubinu.studyhubserver.bookmark.repository.BookmarkRepository;
 import kr.co.studyhubinu.studyhubserver.studypost.domain.StudyPostEntity;
 import kr.co.studyhubinu.studyhubserver.studypost.repository.StudyPostRepository;
+import kr.co.studyhubinu.studyhubserver.support.fixture.BookmarkEntityFixture;
 import kr.co.studyhubinu.studyhubserver.support.fixture.StudyPostEntityFixture;
 import kr.co.studyhubinu.studyhubserver.support.fixture.UserEntityFixture;
 import kr.co.studyhubinu.studyhubserver.user.domain.UserEntity;
@@ -38,7 +39,7 @@ class BookmarkServiceTest {
     private BookmarkService bookmarkService;
 
     @Test
-    void 북마크_생성_성공() {
+    void 북마크_아이디로_북마크가_조회되지_않는다면_북마크를_생성한다() {
         // given
         Long bookmarkUserId = 1L;
         Long bookmarkPostId = 3L;
@@ -59,6 +60,32 @@ class BookmarkServiceTest {
                 () -> assertTrue(result),
                 () -> assertEquals(bookmark.getUserId(), bookmarkUserId),
                 () -> assertEquals(bookmark.getPostId(), bookmarkPostId)
+        );
+    }
+
+    @Test
+    void 북마크_아이디로_북마크_조회되면_북마크를_삭제한다() {
+        // given
+        Long bookmarkUserId = 1L;
+        Long bookmarkPostId = 3L;
+        UserEntity user = UserEntityFixture.DONGWOO.UserEntity_생성(bookmarkUserId);
+        StudyPostEntity post = StudyPostEntityFixture.SQLD.studyPostEntity_생성(bookmarkPostId);
+        BookmarkEntity bookmark = BookmarkEntityFixture.BOOKMARK_POST1.bookMarkEntity_생성(bookmarkPostId, bookmarkUserId);
+
+        given(userRepository.findById(bookmarkUserId)).willReturn(Optional.of(user));
+        given(studyPostRepository.findById(bookmarkPostId)).willReturn(Optional.of(post));
+        given(bookmarkRepository.findByUserIdAndPostId(bookmarkUserId, bookmarkPostId)).willReturn(Optional.of(bookmark));
+        ArgumentCaptor<BookmarkEntity> captor = ArgumentCaptor.forClass(BookmarkEntity.class);
+
+        // when
+        boolean result = bookmarkService.doBookMark(bookmarkUserId, bookmarkPostId);
+        // then
+        verify(bookmarkRepository, times(1)).delete(captor.capture());
+        BookmarkEntity actualBookmark = captor.getValue();
+        assertAll(
+                () -> assertFalse(result),
+                () -> assertEquals(actualBookmark.getUserId(), bookmarkUserId),
+                () -> assertEquals(actualBookmark.getPostId(), bookmarkPostId)
         );
     }
 
