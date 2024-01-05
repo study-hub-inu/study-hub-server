73992f2a6c1060c8fcdf0c85c49142d65b354728
elyudwo <dudqk9696@naver.com>
2024-01-05 15:50:54 +0900

    [Feat] : StudyEntity 식별자로 ApplyEntity 검색 기능 구현

---
+++ src/main/java/kr/co/studyhubinu/studyhubserver/apply/repository/ApplyRepositoryImpl.java
@@ -0,0 +1,35 @@
+package kr.co.studyhubinu.studyhubserver.apply.repository;
+
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import kr.co.studyhubinu.studyhubserver.apply.dto.data.ApplyUserData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static kr.co.studyhubinu.studyhubserver.user.domain.QUserEntity.userEntity;
+import static kr.co.studyhubinu.studyhubserver.apply.domain.QApplyEntity.applyEntity;
+
+@Repository
+@RequiredArgsConstructor
+public class ApplyRepositoryImpl implements ApplyRepositoryCustom {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<ApplyUserData> findByStudy(Long studyId, final Pageable pageable) {
+        return jpaQueryFactory
+                .select(Projections.constructor(ApplyUserData.class,
+                        userEntity.id, userEntity.nickname, userEntity.major, userEntity.imageUrl,
+                        applyEntity.introduce, applyEntity.createdDate))
+                .from(applyEntity)
+                .leftJoin(userEntity).on(applyEntity.userId.eq(userEntity.id))
+                .where(applyEntity.studyId.eq(studyId))
+                .orderBy(applyEntity.createdDate.asc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageNumber() + 1)
+                .fetch();
+    }
+}
