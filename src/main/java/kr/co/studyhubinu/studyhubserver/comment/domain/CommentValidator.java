e29c810a8d222e1199e19499ab6b0577a8b184fa
wellbeing-dough <kdw990202@naver.com>
2024-01-07 23:48:50 +0900

    [Test] : 댓글 로그 추가

--- src/main/java/kr/co/studyhubinu/studyhubserver/comment/domain/CommentValidator.java
+++ src/main/java/kr/co/studyhubinu/studyhubserver/comment/domain/CommentValidator.java
@@ -2,13 +2,17 @@ package kr.co.studyhubinu.studyhubserver.comment.domain;
 
 import kr.co.studyhubinu.studyhubserver.exception.user.UserNotAccessRightException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class CommentValidator {
 
     public void validIsCommentOfUser(Long userId, CommentEntity comment) {
+        log.info("*****************" + userId);
+        log.info("%%%%%%%%%%%%%%%%%" + comment.getUserId());
         if (!comment.isCommentOfUser(userId)) throw new UserNotAccessRightException();
     }
 
