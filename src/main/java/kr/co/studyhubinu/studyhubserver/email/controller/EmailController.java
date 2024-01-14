f51d549f8dc20c4f79f2760178df24c798f419c5
wellbeing-dough <kdw990202@naver.com>
2024-01-15 02:37:34 +0900

    [Feat] : 문의하기 기능

--- src/main/java/kr/co/studyhubinu/studyhubserver/email/controller/EmailController.java
+++ src/main/java/kr/co/studyhubinu/studyhubserver/email/controller/EmailController.java
@@ -3,10 +3,12 @@ package kr.co.studyhubinu.studyhubserver.email.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import kr.co.studyhubinu.studyhubserver.email.dto.request.MailValidDuplicationRequest;
 import kr.co.studyhubinu.studyhubserver.email.dto.request.MailValidRequest;
+import kr.co.studyhubinu.studyhubserver.email.dto.request.QuestionRequest;
 import kr.co.studyhubinu.studyhubserver.email.dto.response.ValidEmailResponse;
 import kr.co.studyhubinu.studyhubserver.email.service.EmailService;
 import kr.co.studyhubinu.studyhubserver.email.dto.request.MailSendRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,7 +24,7 @@ public class EmailController {
 
     @Operation(summary = "이메일 인증코드 전송", description = "바디에 {email} json 형식으로 보내주시면 됩니다. ")
     @PostMapping("/v1/email")
-    public ResponseEntity<Void> sendEmail(@Valid @RequestBody MailSendRequest request) throws MessagingException {
+    public ResponseEntity<HttpStatus> sendEmail(@Valid @RequestBody MailSendRequest request) throws MessagingException {
         emailService.sendEmail(request.toService());
         return ResponseEntity.ok().build();
     }
@@ -36,8 +38,15 @@ public class EmailController {
 
     @Operation(summary = "이메일 중복 검사", description = "")
     @PostMapping("/v1/email/duplication")
-    public ResponseEntity<Void> validDuplication(@Valid @RequestBody MailValidDuplicationRequest request) {
+    public ResponseEntity<HttpStatus> validDuplication(@Valid @RequestBody MailValidDuplicationRequest request) {
         emailService.validDuplication(request.toService());
+        return ResponseEntity.ok().build();
+    }
+
+    @Operation(summary = "문의하기", description = "문의하면 저희 이메일로 오게 했습니다")
+    @PostMapping("/v1/email/question")
+    public ResponseEntity<HttpStatus> question(@Valid @RequestBody final QuestionRequest questionRequest) throws MessagingException {
+        emailService.sendQuestionEmail(questionRequest);
         return ResponseEntity.ok().build();
     }
 
--- src/test/java/kr/co/studyhubinu/studyhubserver/support/repository/RepositoryTest.java
+++ src/test/java/kr/co/studyhubinu/studyhubserver/support/repository/RepositoryTest.java
@@ -13,7 +13,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
-@AutoConfigureTestDatabase(replace = Replace.NONE)
+@AutoConfigureTestDatabase(replace = Replace.AUTO_CONFIGURED)
 @DataJpaTest(properties = {"spring.jpa.properties.hibernate.jdbc.time_zone=Asia/Seoul"})
 @Import({TestQueryDslConfig.class, JpaAuditingConfig.class})
 public @interface RepositoryTest {
