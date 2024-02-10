8209e19149c2078ade6532e2c93e519b5eba99a1
wellbeing-dough <kdw990202@naver.com>
2024-02-11 00:55:45 +0900

    [Feat] : RejectApplyRequest valid추가

--- src/main/java/kr/co/studyhubinu/studyhubserver/apply/dto/request/RejectApplyRequest.java
+++ src/main/java/kr/co/studyhubinu/studyhubserver/apply/dto/request/RejectApplyRequest.java
@@ -4,12 +4,18 @@ import kr.co.studyhubinu.studyhubserver.apply.domain.RejectEntity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @NoArgsConstructor
 public class RejectApplyRequest {
 
+    @NotNull(message = "스터디 아이디는 필수입니다!")
     private Long studyId;
+    @NotNull(message = "거절된 유저 아이디는 필수입니다!")
     private Long rejectedUserId;
+    @NotBlank(message = "거절 내용 작성은 필수입니다!")
     private String rejectReason;
 
     public RejectApplyRequest(Long studyId, Long rejectedUserId, String rejectReason) {
