8cb188ac258134d350a9d9efd383a26506a3b3a2
Lee Yeung Jae <dudqk9696@naver.com>
2024-02-17 01:51:49 +0900

    [Fix] : 거절 사유 조회 쿼리문 수정

--- src/main/java/kr/co/studyhubinu/studyhubserver/reject/repository/RejectRepositoryImpl.java
+++ src/main/java/kr/co/studyhubinu/studyhubserver/reject/repository/RejectRepositoryImpl.java
@@ -1,27 +1,44 @@
 package kr.co.studyhubinu.studyhubserver.reject.repository;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import kr.co.studyhubinu.studyhubserver.reject.dto.response.RejectReasonResponse;
 import lombok.RequiredArgsConstructor;
 
 import javax.persistence.EntityManager;
-import javax.persistence.Query;
+
+import static kr.co.studyhubinu.studyhubserver.apply.domain.QRejectEntity.rejectEntity;
+import static kr.co.studyhubinu.studyhubserver.study.domain.QStudyEntity.studyEntity;
 
 @RequiredArgsConstructor
 public class RejectRepositoryImpl implements RejectRepositoryCustom {
 
     private final EntityManager em;
+    private final JPAQueryFactory jpaQueryFactory;
 
     @Override
     public RejectReasonResponse findByStudyIdAndRejectedUserId(Long studyId, Long rejectedUserId) {
-        Query nativeQuery = em.createNativeQuery(makeNativeQuery(rejectedUserId));
-        Object[] obj = (Object[]) nativeQuery.getSingleResult();
-
-        return new RejectReasonResponse((String) obj[0], (String) obj[1]);
+        return jpaQueryFactory
+                .select(Projections.constructor(RejectReasonResponse.class,
+                        studyEntity.title, rejectEntity.rejectReason))
+                .from(rejectEntity)
+                .innerJoin(studyEntity)
+                .on(rejectEntity.studyId.eq(studyEntity.id))
+                .where(studyEntity.id.eq(studyId), rejectEntity.rejectedUserId.eq(rejectedUserId))
+                .fetchOne();
     }
 
-    private String makeNativeQuery(Long rejectedUserId) {
-        return "SELECT s.title, r.reject_reason " +
-                "FROM (SELECT * FROM reject WHERE reject.rejected_user_id = " + rejectedUserId + ") as r " +
-                "LEFT JOIN study as s ON s.study_id = r.study_id";
-    }
+//    @Override
+//    public RejectReasonResponse findByStudyIdAndRejectedUserId(Long studyId, Long rejectedUserId) {
+//        Query nativeQuery = em.createNativeQuery(makeNativeQuery(rejectedUserId));
+//        Object[] obj = (Object[]) nativeQuery.getSingleResult();
+//
+//        return new RejectReasonResponse((String) obj[0], (String) obj[1]);
+//    }
+
+//    private String makeNativeQuery(Long rejectedUserId) {
+//        return "SELECT s.title, r.reject_reason " +
+//                "FROM (SELECT * FROM reject WHERE reject.rejected_user_id = " + rejectedUserId + ") as r " +
+//                "LEFT JOIN study as s ON s.study_id = r.study_id";
+//    }
 }
