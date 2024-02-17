c827377933af2cbf0e02541f67eb5a40d406f0ed
Lee Yeung Jae <dudqk9696@naver.com>
2024-02-17 17:36:01 +0900

    [Refactor] : 스터디 신청 삭제 기능 쿼리파라미터 형태로 변경

--- src/main/java/kr/co/studyhubinu/studyhubserver/apply/controller/ApplyController.java
+++ src/main/java/kr/co/studyhubinu/studyhubserver/apply/controller/ApplyController.java
@@ -81,9 +81,8 @@ public class ApplyController {
     }
 
     @Operation(summary = "내가 신청한 스터디 삭제", description = "헤더에 JWT와 함께 studyId를 보내주시면 됩니다.")
-    @ApiImplicitParam(name = "studyId", value = "스터디 식별자", required = true)
-    @DeleteMapping("/v1/study")
-    public ResponseEntity<HttpStatus> deleteApply(UserId userId, Long studyId) {
+    @DeleteMapping("/v1/study/{studyId}")
+    public ResponseEntity<HttpStatus> deleteApply(UserId userId, @PathVariable("studyId") Long studyId) {
         applyService.deleteMyStudy(userId, studyId);
         return ResponseEntity.ok().build();
     }
