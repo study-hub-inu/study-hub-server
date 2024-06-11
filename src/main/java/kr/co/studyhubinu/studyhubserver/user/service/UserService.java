e90e0470e2c80eaf77c3fddc910c2f74e3268335
wellbeing-dough <kdw990202@naver.com>
2024-06-11 19:31:56 +0900

    [Test] : CICD 테스트

--- src/main/java/kr/co/studyhubinu/studyhubserver/user/service/UserService.java
+++ src/main/java/kr/co/studyhubinu/studyhubserver/user/service/UserService.java
@@ -18,7 +18,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-
 @Slf4j
 @RequiredArgsConstructor
 @Service
